589322d797800a510abe0d46ca17e894ce327008
suecharo <suehiro619@gmail.com>
2023-02-13 17:58:41 +0900

    Add impl

--- README.md
+++ README.md
@@ -2,7 +2,113 @@
 
 Experimenting using Flask and [Flask-Executor](https://flask-executor.readthedocs.io/en/latest/).
 
+## Environment
+
 ```bash
 $ docker compose up -d --build
 $ docker compose exec app bash
+```
+
+## Example
+
+```bash
+$ python3 ./flask_exec_exp/app.py
+ * Serving Flask app 'app'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on all addresses (0.0.0.0)
+ * Running on http://127.0.0.1:8080
+ * Running on http://172.19.0.2:8080
+Press CTRL+C to quit
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 679-794-273
+```
+
+### Running and complete
+
+```bash
+$ curl -X POST localhost:8080/validate -H "Content-Type: application/json" -d '{"arg1": "foo", "arg2": "bar"}'
+{
+  "requestId": "721a6ef3-7872-454e-ae5b-3be6ecb2e815"
+}
+
+$ curl -X GET localhost:8080/721a6ef3-7872-454e-ae5b-3be6ecb2e815
+{
+  "request": {
+    "arg1": "foo",
+    "arg2": "bar"
+  },
+  "requestId": "721a6ef3-7872-454e-ae5b-3be6ecb2e815",
+  "results": [],
+  "status": "RUNNING"
+}
+
+$ curl -X GET localhost:8080/721a6ef3-7872-454e-ae5b-3be6ecb2e815
+{
+  "request": {
+    "arg1": "foo",
+    "arg2": "bar"
+  },
+  "requestId": "721a6ef3-7872-454e-ae5b-3be6ecb2e815",
+  "results": [
+    {
+      "key": "arg1",
+      "val": "foo"
+    },
+    {
+      "key": "arg2",
+      "val": "bar"
+    }
+  ],
+  "status": "COMPLETE"
+}
+```
+
+### Cancel
+
+```bash
+$ curl -X POST localhost:8080/validate -H "Content-Type: application/json" -d '{"arg1": "foo", "arg2": "bar"}'
+{
+  "requestId": "ba66637c-3ec7-4e96-afa8-f79b757d18d8"
+}
+$ curl -X GET localhost:8080/ba66637c-3ec7-4e96-afa8-f79b757d18d8
+{
+  "request": {
+    "arg1": "foo",
+    "arg2": "bar"
+  },
+  "requestId": "ba66637c-3ec7-4e96-afa8-f79b757d18d8",
+  "results": [],
+  "status": "RUNNING"
+}
+$ curl -X POST localhost:8080/ba66637c-3ec7-4e96-afa8-f79b757d18d8/cancel
+<!doctype html>
+<html lang=en>
+<title>400 Bad Request</title>
+<h1>Bad Request</h1>
+<p>Failed to cancel</p>
+```
+
+### Error
+
+```bash
+$ curl -X POST localhost:8080/validate -H "Content-Type: application/json" -d '{"error": "foobar"}'
+{
+  "requestId": "3b4f97f2-02a3-4b9c-92eb-388cb662c2cd"
+}
+
+$ curl -X GET localhost:8080/3b4f97f2-02a3-4b9c-92eb-388cb662c2cd
+{
+  "request": {
+    "error": "foobar"
+  },
+  "requestId": "3b4f97f2-02a3-4b9c-92eb-388cb662c2cd",
+  "results": [
+    {
+      "err_msg": "This is our implemented error: foobar"
+    }
+  ],
+  "status": "FAILED"
+}
 ```
--- flask_exec_exp/app.py
+++ flask_exec_exp/app.py
@@ -1,5 +1,149 @@
 #!/usr/bin/env python3
 # coding: utf-8
+import time
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, Dict, List
+from uuid import uuid4
+
+from flask import Blueprint, Flask, Response, abort, current_app, request
+from flask.json import jsonify
+
+GET_STATUS_CODE = 200
+POST_STATUS_CODE = 200
+JOB_STATUS = [
+    "UNKNOWN",
+    "QUEUED",
+    "RUNNING",
+    "COMPLETE",
+    "FAILED",
+    "CANCELING",
+    "CANCELED",
+]
+
+
+# --- state ---
+
+
+executor = ThreadPoolExecutor(max_workers=3)
+job_map = {}
+request_map = {}
+
+
+# --- controller ---
+
+
+app_bp = Blueprint("app", __name__)
+
+
+@app_bp.route("/validate", methods=["POST"])
+def validate() -> Response:
+    req_id = str(uuid4())
+    req_body = request.json or {}
+    with current_app.app_context():
+        request_map[req_id] = req_body
+
+        # add job to queue
+        job = executor.submit(base_job, req_body)
+        job_map[req_id] = job
+
+    response: Response = jsonify({"requestId": req_id})
+    response.status_code = POST_STATUS_CODE
+
+    return response
+
+
+@app_bp.route("/<string:req_id>", methods=["GET"])
+def get_results(req_id: str) -> Response:
+    if req_id not in job_map:
+        abort(400, "requestId not found")
+    job = job_map[req_id]
+    request = request_map[req_id]
+
+    # check status
+    status = "UNKNOWN"
+    results = []
+    if job.running():
+        status = "RUNNING"
+    elif job.cancelled():
+        status = "CANCELED"
+    elif job.done():
+        # COMPLETE or FAILED
+        if job.exception() is None:
+            status = "COMPLETE"
+            # TODO: The expected original results are a list of exceptions (or a exception), however here I assume the results are a list of dicts. (which means we need to write a wrapper)
+            results = job.result()
+        else:
+            status = "FAILED"
+            results = [{"err_msg": str(job.exception())}]
+
+    response: Response = jsonify({
+        "requestId": req_id,
+        "request": request,
+        "status": status,
+        "results": results
+    })
+    response.status_code = GET_STATUS_CODE
+
+    return response
+
+
+@app_bp.route("/<string:req_id>/cancel", methods=["POST"])
+def cancel(req_id: str) -> Response:
+    if req_id not in job_map:
+        abort(400, "requestId not found")
+    job = job_map[req_id]
+    try_cancel = job.cancel()
+    if not try_cancel:
+        abort(400, "Failed to cancel")
+    response: Response = jsonify({"requestId": req_id})
+    response.status_code = POST_STATUS_CODE
+
+    return response
+
+
+# --- job ---
+
+
+def base_job(request: Dict[str, Any]) -> List[Dict[str, Any]]:  # type: ignore
+    if "error" in request:
+        error_job(request)
+    else:
+        return some_long_job(request)
+
+
+def some_long_job(request: Dict[str, Any]) -> List[Dict[str, Any]]:
+    time.sleep(60)
+
+    return [{"key": k, "val": v} for k, v in request.items()]
+
+
+def error_job(request: Dict[str, Any]) -> None:
+    raise Exception(f"This is our implemented error: {request['error']}")
+
+
+# --- app ---
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(app_bp)
+
+    # for debug
+    app.config["FLASK_ENV"] = "development"
+    app.config["DEBUG"] = True
+    app.config["TESTING"] = True
+
+    return app
+
+
+def main() -> None:
+    app = create_app()
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        debug=True,
+    )
+
 
 if __name__ == "__main__":
-    pass
+    main()
